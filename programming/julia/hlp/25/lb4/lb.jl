f09b8c9cd90698fe94bdfea15bcf61b251d61bad
Neo <neuronsmatrix@gmail.com>
2023-01-14 18:01:27 +0300

    25 var push l4,l3

--- programming/julia/hlp/25/lb4/lb.jl
+++ programming/julia/hlp/25/lb4/lb.jl
@@ -4,6 +4,9 @@ close(input)
 s = size(lines)[1]
 open("output.txt","w") do f
     for l in 1:(s)
-        write(f,lines[l])
+        if (lines[l][1]==lines[l][length(lines[l])])
+            write(f,lines[l])
+            write(f,'\n')
+        end
     end
 end
